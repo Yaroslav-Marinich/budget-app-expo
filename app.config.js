dc3a00800ad0584dbe8141874c0a0de24ad6721b
Yaroslav <yaroslav-frontend@yobet.game>
2026-05-12 21:37:22 +0300

    pre-relise config

---
+++ app.config.js
@@ -0,0 +1,67 @@
+const IS_DEV = process.env.EAS_BUILD_PROFILE === 'development';
+
+export default {
+  expo: {
+    name: IS_DEV ? "СімБюджет (Dev)" : "СімБюджет",
+    slug: "budget-app",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./src/assets/images/icon.png",
+    notification: {
+      icon: "./src/assets/images/notification-icon.png",
+      color: "#1B5E20"
+    },
+    scheme: "budget-app",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      package: IS_DEV ? "com.simbudget.budgetapp.dev" : "com.simbudget.budgetapp",
+      versionCode: 1,
+      version: "1.0.0",
+      googleServicesFile: "./google-services.json",
+      adaptiveIcon: {
+        foregroundImage: "./src/assets/images/icon.png",
+        backgroundColor: "#ffffff"
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false
+    },
+    web: {
+      output: "static",
+      favicon: "./assets/images/favicon.png"
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          "image": "./src/assets/images/logo.png",
+          "imageWidth": 200,
+          "resizeMode": "contain",
+          "backgroundColor": "#ffffff",
+          "dark": {
+            "backgroundColor": "#121212"
+          }
+        }
+      ],
+      "expo-font",
+      "expo-image",
+      "expo-web-browser",
+      "expo-sharing",
+      "@react-native-google-signin/google-signin"
+    ],
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true
+    },
+    extra: {
+      router: {},
+      eas: {
+        projectId: "b9d31e56-5bcd-4454-a1ec-c4f23749b423"
+      }
+    }
+  }
+};
